b1334d4935ddecbd5aaee2107fd4c54102b4d692
cngocvu <cngocvu@gmail.com>
2014-06-04 14:07:50 +0700

    chinh

--- src/com/example/ishoopingcaffe/SearchActivity.java
+++ src/com/example/ishoopingcaffe/SearchActivity.java
@@ -4,7 +4,7 @@ import com.example.listener.OnEventControlListener;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
+/*import android.support.v7.app.ActionBarActivity;*/
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
